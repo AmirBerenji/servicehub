7936cec066482f87c73801d12be5f384e6cd2afd
AmirBerenji <a.berenji62@gmail.com>
2026-06-04 12:16:30 +0400

    Create the api for category

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,9 +24,42 @@ class DatabaseSeeder extends Seeder
         ]);
 
         DB::table('roles')->insert([
-            ['name' => 'gust'],
+            ['name' => 'guest'],
             ['name' => 'provider'],
             ['name' => 'admin'],
+        ]);
+
+        DB::table('categories')->insert([
+            ['name' => 'Beauty','description' => 'Beauty','icon'=>'Sparkles',
+                'activeClass'=>'border-rose-400 bg-rose-50 text-rose-700',
+                'chipClass'=>'bg-rose-100 text-rose-700'],
+            ['name' => 'Cleaning','description' => 'Cleaning','icon'=>'SprayCan',
+                'activeClass'=>'border-teal-500 bg-teal-50 text-teal-700',
+                'chipClass'=>'bg-teal-100 text-teal-700'],
+            ['name' => 'Car Service','description' => 'Car Service','icon'=>'Car',
+                'activeClass'=>'border-orange-500 bg-orange-50 text-orange-700',
+                'chipClass'=>'bg-orange-100 text-orange-700'],
+        ]);
+
+        DB::table('services')->insert([
+            ['category_id' => '1','name' => 'Hair styling'],
+            ['category_id' => '1','name' => 'Nails'],
+            ['category_id' => '1','name' => 'Skincare'],
+            ['category_id' => '1','name' => 'Makeup'],
+            ['category_id' => '1','name' => 'Waxing'],
+
+            ['category_id' => '2','name' => 'Home cleaning'],
+            ['category_id' => '2','name' => 'Office cleaning'],
+            ['category_id' => '2','name' => 'Deep cleaning'],
+            ['category_id' => '2','name' => 'Carpet'],
+            ['category_id' => '2','name' => 'Windows'],
+
+            ['category_id' => '3','name' => 'Oil change'],
+            ['category_id' => '3','name' => 'Diagnostics'],
+            ['category_id' => '3','name' => 'Car wash'],
+            ['category_id' => '3','name' => 'Tire service'],
+            ['category_id' => '3','name' => 'Towing'],
+
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,15 +3,20 @@
 
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\BusinessController;
+use App\Http\Controllers\API\CategoryController;
 
 Route::prefix('user')->group(function () {
     // Public routes
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
 
-
     // Protected routes
     Route::middleware('auth:sanctum')->group(function () {
-
+        Route::get('profile', [AuthController::class, 'user']);
     });
+});
+
+Route::prefix('category')->group(function () {
+    Route::get('all', [CategoryController::class, 'index']);
+
 });
